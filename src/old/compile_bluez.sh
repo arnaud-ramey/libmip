d9f058e367d533a02ba0ecb32ab7aa88cd894f0b
arnaud.a.ramey@gmail.com <arnaud.a.ramey@gmail.com@214c0f9e-170b-11df-9ac1-3b3d05a0ff07>
2015-03-14 15:28:03 +0000

    cleaning

--- src/old/compile_bluez.sh
+++
@@ -1,7 +0,0 @@
- ./configure --prefix=/usr --mandir=/usr/share/man --sysconfdir=/etc --localstatedir=/var --enable-experimental --with-systemdsystemunitdir=/lib/systemd/system --with-systemduserunitdir=/usr/lib/systemd --enable-library
-
-arnaud@eeepc:~/Dropbox/code/ramey/mip$ ls /usr/lib/libbluetooth.* -al
--rwxr-xr-x 1 root root    952 janv. 10 23:44 /usr/lib/libbluetooth.la
-lrwxrwxrwx 1 root root     22 janv. 10 23:44 /usr/lib/libbluetooth.so -> libbluetooth.so.3.18.0
-lrwxrwxrwx 1 root root     22 janv. 10 23:44 /usr/lib/libbluetooth.so.3 -> libbluetooth.so.3.18.0
--rwxr-xr-x 1 root root 315243 janv. 10 23:44 /usr/lib/libbluetooth.so.3.18.0
